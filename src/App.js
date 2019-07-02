02182f7e7b29b8ae0c636789a6640dfbdcc6adb4
Work_laptop_Alex <alexmacdon94@gmail.com>
2019-07-02 11:10:09 -0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Titles from "./components/Titles"
+import Form from "./components/Form"
+import Hubster from "./components/Hubster"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class FrontEnd extends React.Component{
+  getHubsters = async () => {
+    const api_call = await fetch('http://santamonicahubsters.ddns.net:5000/api/husters'); 
+  }
+  render(){
+    return(
+      <div>Hello!
+      <p>Hello!</p>
+      <h1>YO!</h1>
+        <Titles />
+        <Form />
+        <Hubster />
+      </div>
+
+    );
+  }
 }
 
-export default App;
+
+export default FrontEnd;
---
+++ src/components/Hubster.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+class Titles extends React.Component {
+  render(){
+    return (
+      <div>
+        <h1>Hubster Finder</h1>
+        <p>Find unique hubster, and his/her info</p>
+      </div>
+    )
+  }
+}
+
+export default Titles
